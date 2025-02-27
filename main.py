c9a514d981c7cccb0aad634c5ddbcf147ee264a4
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 23:01:05 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from colorama import Fore
+
+green = Fore.GREEN
+reset = Fore.RESET
+red = Fore.BLUE
+yellow = Fore.YELLOW
+blue = Fore.BLUE
+white = Fore.WHITE
+
+mbanner = f'''
+{red} ███▄ ▄███▓ █    ██  ██▓  ▄▄▄█████▓ ██▓▓█████▄ ▓█████▄  ▒█████    ██████ 
+▓██▒▀█▀ ██▒ ██  ▓██▒▓██▒  ▓  ██▒ ▓▒▓██▒▒██▀ ██▌▒██▀ ██▌▒██▒  ██▒▒██    ▒ 
+▓██    ▓██░▓██  ▒██░▒██░  ▒ ▓██░ ▒░▒██▒░██   █▌░██   █▌▒██░  ██▒░ ▓██▄   
+▒██    ▒██ ▓▓█  ░██░▒██░  ░ ▓██▓ ░ ░██░░▓█▄   ▌░▓█▄   ▌▒██   ██░  ▒   ██▒
+{yellow}▒██▒   ░██▒▒▒█████▓ ░██████▒▒██▒ ░ ░██░░▒████▓ ░▒████▓ ░ ████▓▒░▒██████▒▒
+░ ▒░   ░  ░░▒▓▒ ▒ ▒ ░ ▒░▓  ░▒ ░░   ░▓   ▒▒▓  ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░
+░  ░      ░░░▒░ ░ ░ ░ ░ ▒  ░  ░     ▒ ░ ░ ▒  ▒  ░ ▒  ▒   ░ ▒ ▒░ ░ ░▒  ░ ░
+░      ░    ░░░ ░ ░   ░ ░   ░       ▒ ░ ░ ░  ░  ░ ░  ░ ░ ░ ░ ▒  ░  ░  ░  
+       ░      ░         ░  ░        ░     ░       ░        ░ ░        ░  
+                                        ░       ░                    {reset}
+                {blue}/=====================================/
+                || {yellow}[+] {green}Author: {white}t.me/neonworid        {blue}||
+                || {yellow}[+] {green}GitHub: {white}github.com/Sakuzuna   {blue}||
+                || {yellow}[+] {green}Team: {white}LDDoS [skid]            {blue}||
+                || {yellow}[+] {green}Version: {white}V.1                  {blue}||
+                /=====================================/ {reset}'''
+
+print(mbanner)
